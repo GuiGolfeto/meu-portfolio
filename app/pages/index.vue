81a8b77d8f4562869627da8a3736b9d2cdcbe135
GuiGolfeto <ggfguigolfeto@gmail.com>
2025-10-28 09:09:23 -0300

    alteração de email

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -763,7 +763,7 @@ const plans: Plan[] = [
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
-                            <a :href="mailLink" class="hover:underline">golfeto.dev@gmail.com</a>
+                            <a :href="mailLink" class="hover:underline">contato@webspark.com.br</a>
                         </div>
                         <div class="flex items-start">
                             <svg class="w-5 h-5 mr-3 text-blue-400 mt-1" fill="none" stroke="currentColor"
